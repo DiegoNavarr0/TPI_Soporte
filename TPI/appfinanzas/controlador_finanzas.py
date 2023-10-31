8d53b771a155580dcfb44cfe5f335376378c9177
DiegoNavarr0 <navarrodiego201513@gmail.com>
2023-10-31 14:40:14 -0300

    Implementados el inicio, las transacciones, wishes y gastos fijos

--- TPI/appfinanzas/controlador_finanzas.py
+++ TPI/appfinanzas/controlador_finanzas.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from conector_db import obtener_conexion
 
 def obtener_saldo(id_usuario):
@@ -7,7 +6,7 @@ def obtener_saldo(id_usuario):
     cursor = conexion.cursor()
 
     query = "SELECT saldo FROM usuario WHERE id_usuario = %s"
-    cursor.execute(query, (id_usuario,))
+    cursor.execute(query, (id_usuario))
     saldo = cursor.fetchone()[0]
     conexion.close()
     return saldo
@@ -16,7 +15,7 @@ def obtener_historial_transacciones(id_usuario):
     conexion = obtener_conexion()
     cursor = conexion.cursor()
 
-    query = "SELECT id_transaccion, descripcion, monto FROM operacion WHERE id_usuario = %s"
+    query = "SELECT id_transaccion, descripcion, monto, fecha_hora FROM operacion WHERE id_usuario = %s"
     cursor.execute(query, (id_usuario,))
     historial = cursor.fetchall()
     conexion.close()
@@ -35,7 +34,7 @@ def insertar_usuario(usuario, password):
         conexion.close()
         return True  # User registration successful
     except conexion.Error as err:
-        print("Error inserting user:", err)
+        print("Error registrando usuario:", err)
         conexion.rollback()
         conexion.close()
         return False  # User registration failed
@@ -47,24 +46,11 @@ def validar_usuario(usuario, password):
     query = "SELECT id_usuario FROM usuario WHERE usuario = %s AND pwd = %s"
     cursor.execute(query, (usuario, password))
     result = cursor.fetchone()
-    print("Authentication result:", result)  # Para debugging
     conexion.close()
     if result is not None:
         return result
     else:
         return False
-
-def obtener_usuarios_registrados():
-    conexion = obtener_conexion()
-    cursor = conexion.cursor()
-
-    query = "SELECT id_usuario, usuario FROM usuario"
-    cursor.execute(query)
-    usuarios = cursor.fetchall()
-    cursor.close()
-    conexion.close()
-    return usuarios
-
 
 def insertar_transaccion(id_usuario, descripcion, monto, tipo_transaccion):
     conexion = obtener_conexion()
@@ -76,23 +62,185 @@ def insertar_transaccion(id_usuario, descripcion, monto, tipo_transaccion):
     values = (id_usuario, tipo_transaccion, descripcion, monto, fecha_hora)
 
     query_modificar_saldo = None  # Initialize the query_modificar_saldo variable
-
     if tipo_transaccion == 1:  # Ingreso
         query_modificar_saldo = "UPDATE usuario SET saldo = saldo + %s WHERE id_usuario = %s"
     elif tipo_transaccion == 2:  # Egreso
         query_modificar_saldo = "UPDATE usuario SET saldo = saldo - %s WHERE id_usuario = %s"
-
+    print(query_modificar_saldo)
     try:
         cursor.execute(query, values)
         if query_modificar_saldo is not None:
+            print(monto, id_usuario)
             cursor.execute(query_modificar_saldo, (monto, id_usuario))
-
         conexion.commit()
         conexion.close()
         return True
     except conexion.Error as err:
-        print("Error inserting transaction:", err)
+        print("Error agregando transaccion:", err)
         conexion.rollback()
         conexion.close()
         return False
 
+def agregar_wish(id_usuario, motivo, monto):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    fecha_creacion = datetime.now().date()
+
+    query = "INSERT INTO wish (id_usuario, motivo, monto, fecha_creacion) VALUES (%s, %s, %s, %s)"
+    values = (id_usuario, motivo, monto, fecha_creacion)
+
+    try:
+        cursor.execute(query, values)
+        conexion.commit()
+        conexion.close()
+        return True
+    except conexion.Error as err:
+        print("Error agregando wish:", err)
+        conexion.rollback()
+        conexion.close()
+        return False
+
+def obtener_wishes(id_usuario):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    query = "SELECT * FROM wish WHERE id_usuario = %s"
+    cursor.execute(query, (id_usuario))
+    wishes = cursor.fetchall()
+    conexion.close()
+    return wishes
+
+def editar_wish(id_usuario, id_wish, motivo, monto):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    query = "UPDATE wish SET motivo = %s, monto = %s WHERE id_wish = %s AND id_usuario = %s"
+    values = (motivo, monto, id_wish, id_usuario)
+
+    try:
+        cursor.execute(query, values)
+        conexion.commit()
+        conexion.close()
+        return True
+    except conexion.Error as err:
+        print("Error editando wish:", err)
+        conexion.rollback()
+        conexion.close()
+        return False
+
+def obtener_wish(id_wish):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    query = "SELECT * FROM wish WHERE id_wish = %s"
+    cursor.execute(query, (id_wish,))
+    result = cursor.fetchone()
+    conexion.close()
+    return result
+
+def eliminar_wish(id_wish):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    try:
+        cursor.execute("SELECT id_wish FROM wish WHERE id_wish = %s", id_wish)
+        if cursor.fetchone():
+            cursor.execute("DELETE FROM wish WHERE id_wish = %s", (id_wish,))
+            conexion.commit()
+            conexion.close()
+            return True
+        else:
+            conexion.close()
+            return False
+    except conexion.Error as err:
+        print("Error eliminando wish:", err)
+        conexion.rollback()
+        conexion.close()
+        return False
+
+def obtener_gastos(id_usuario):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    query = "SELECT * FROM gastosfijos WHERE id_usuario = %s"
+    cursor.execute(query, id_usuario)
+    gastos = cursor.fetchall()
+    conexion.close()
+    return gastos
+
+def eliminar_gasto_fijo(id_gasto_fijo):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    try:
+        cursor.execute("SELECT id_gastosfijos FROM gastosfijos WHERE id_gastosfijos = %s", id_gasto_fijo)
+        if cursor.fetchone():
+            cursor.execute("DELETE FROM gastosfijos WHERE id_gastosfijos = %s", (id_gasto_fijo,))
+            conexion.commit()
+            conexion.close()
+            return True
+        else:
+            conexion.close()
+            return False
+    except conexion.Error as err:
+        print("Error eliminando gasto fijo:", err)
+        conexion.rollback()
+        conexion.close()
+        return False
+
+def editar_gasto_fijo(id_usuario, id_gastosfijos, descripcion, monto, fecha_vencimiento):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    query = "UPDATE gastosfijos SET descripcion = %s, monto = %s, fecha_vencimiento = %s WHERE id_gastosfijos = %s AND id_usuario = %s"
+    values = (descripcion, monto, fecha_vencimiento, id_gastosfijos, id_usuario)
+
+    try:
+        cursor.execute(query, values)
+        conexion.commit()
+        conexion.close()
+        return True
+    except conexion.Error as err:
+        print("Error editando gasto fijo:", err)
+        conexion.rollback()
+        conexion.close()
+        return False
+
+def obtener_gasto_fijo(id_gastosfijos):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    query = "SELECT * FROM gastosfijos WHERE id_gastosfijos = %s"
+    cursor.execute(query, (id_gastosfijos,))
+    result = cursor.fetchone()
+    conexion.close()
+    return result
+
+def agregar_gasto_fijo(id_usuario, descripcion, monto, fecha_vencimiento):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+    query = "INSERT INTO gastosfijos (id_usuario, descripcion, monto, fecha_vencimiento) VALUES (%s, %s, %s, %s)"
+    values = (id_usuario, descripcion, monto, fecha_vencimiento)
+    try:
+        cursor.execute(query, values)
+        conexion.commit()
+        conexion.close()
+        return True
+    except conexion.Error as err:
+        print("Error agregando gasto fijo:", err)
+        conexion.rollback()
+        conexion.close()
+        return False
+
+
+def obtener_nombre_usuario(id_usuario):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    query = "SELECT usuario FROM usuario WHERE id_usuario = %s"
+    cursor.execute(query, id_usuario)
+    result = cursor.fetchone()
+    nombre_usuario = result[0]
+    conexion.close()
+    return nombre_usuario
--- TPI/appfinanzas/main.py
+++ TPI/appfinanzas/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, session
+from datetime import datetime
+
+from flask import Flask, render_template, request, redirect, session, flash
 import controlador_finanzas
 
 app= Flask(__name__)
@@ -40,11 +42,12 @@ def registro():
 def inicio():
     if 'id_usuario' in session:
         id_usuario = session['id_usuario']
-        saldo_actual = controlador_finanzas.obtener_saldo(id_usuario)  # Reemplaza '1' con el ID del usuario actual
-        historial = controlador_finanzas.obtener_historial_transacciones(id_usuario)  # Reemplaza '1' con el ID del usuario actual
+        saldo_actual = controlador_finanzas.obtener_saldo(id_usuario)
+        historial = controlador_finanzas.obtener_historial_transacciones(id_usuario)
+        nombre_usuario = controlador_finanzas.obtener_nombre_usuario(id_usuario)
 
         # Renderiza la plantilla HTML e incluye el saldo actual y el historial de transacciones como datos
-        return render_template('inicio.html', saldo_actual=saldo_actual, historial=historial)
+        return render_template('inicio.html', usuario= nombre_usuario, saldo_actual=saldo_actual, historial=historial)
     else:
         return redirect('/')
 
@@ -55,7 +58,7 @@ def agregar_transaccion():
             id_usuario = session['id_usuario']
             descripcion = request.form.get('descripcion')
             monto = float(request.form.get('monto'))
-            tipo_transaccion = request.form.get('tipo_transaccion')
+            tipo_transaccion = int(request.form.get('tipo_transaccion'))
 
             if controlador_finanzas.insertar_transaccion(id_usuario, descripcion, monto, tipo_transaccion):
                 return redirect('/inicio')
@@ -65,10 +68,117 @@ def agregar_transaccion():
     else:
         return render_template('agregar_transaccion.html')
 
-@app.route('/usuarios')
-def mostrar_usuarios():
-    usuarios = controlador_finanzas.obtener_usuarios_registrados()
-    return render_template('usuarios.html', usuarios=usuarios)
+@app.route('/wish')
+def wish():
+    if 'id_usuario' in session:
+        id_usuario = session['id_usuario']
+        wishes = controlador_finanzas.obtener_wishes(id_usuario)
+        return render_template('wish.html', wishes=wishes)
+    else:
+        return redirect('/')
+
+@app.route('/agregar_wish', methods=['GET', 'POST'])
+def agregar_wish():
+    if request.method == 'POST':
+        if 'id_usuario' in session:
+            id_usuario = session['id_usuario']
+            motivo = request.form.get('motivo')
+            monto = float(request.form.get('monto'))
+
+            if controlador_finanzas.agregar_wish(id_usuario, motivo, monto):
+                return redirect('/wish')
+            else:
+                return render_template('wish.html', error="Error al agregar el wish")
+
+    else:
+        return render_template('agregar_wish.html')
+
+
+
+@app.route('/editar_wish/<int:id_wish>', methods=['GET', 'POST'])
+def editar_wish(id_wish):
+    if 'id_usuario' in session:
+        if request.method == 'POST':
+            id_usuario = session['id_usuario']
+            motivo = request.form.get('motivo')
+            monto = float(request.form.get('monto'))
+
+            if controlador_finanzas.editar_wish(id_usuario, id_wish, motivo, monto):
+                return redirect('/wish')
+            else:
+                return render_template('editar_wish.html', error="Error al editar el wish")
+        else:
+            wish_obtenido = controlador_finanzas.obtener_wish(id_wish)
+            if wish_obtenido:
+                return render_template('editar_wish.html', wish=wish_obtenido)
+            else:
+                return redirect('/wish')
+    else:
+        return redirect('/')
+
+@app.route('/eliminar_wish/<int:id_wish>', methods=['GET'])
+def eliminar_wish(id_wish):
+    if controlador_finanzas.eliminar_wish(id_wish):
+        return redirect('/wish')
+    else:
+        flash("Error eliminando wish", "error")
+        return redirect('/wish')
+
+@app.route('/gastos_fijos', methods=['GET', 'POST'])
+def gastos_fijos():
+    if 'id_usuario' in session:
+        id_usuario = session['id_usuario']
+        gastos = controlador_finanzas.obtener_gastos(id_usuario)
+        return render_template('gastos_fijos.html', gastos=gastos)
+    else:
+        return redirect('/')
+
+@app.route('/agregar_gasto_fijo', methods=['GET', 'POST'])
+def agregar_gasto_fijo():
+    if request.method == 'POST':
+        if 'id_usuario' in session:
+            id_usuario = session['id_usuario']
+            descripcion = request.form.get('descripcion')
+            monto = float(request.form.get('monto'))
+            fecha_vencimiento = datetime.strptime(request.form.get('fecha_vencimiento'), '%d')
+
+            if controlador_finanzas.agregar_gasto_fijo(id_usuario, descripcion, monto, fecha_vencimiento):
+                return redirect('/gastos_fijos')
+            else:
+                return render_template('gastos_fijos.html', error="Error al agregar el wish")
+
+    else:
+        return render_template('agregar_gasto_fijo.html')
+
+@app.route('/editar_gasto_fijo/<int:id_gastosfijos>', methods=['GET', 'POST'])
+def editar_gasto_fijo(id_gastosfijos):
+    if 'id_usuario' in session:
+        if request.method == 'POST':
+            id_usuario = session['id_usuario']
+            descripcion = request.form.get('descripcion')
+            monto = float(request.form.get('monto'))
+            fecha_vencimiento = datetime.strptime(request.form.get('fecha_vencimiento'), '%d')
+
+            if controlador_finanzas.editar_gasto_fijo(id_usuario, id_gastosfijos, descripcion, monto, fecha_vencimiento):
+                return redirect('/gastos_fijos')
+            else:
+                return render_template('editar_gastos_fijos.html', error="Error al editar el gasto fijo")
+        else:
+            gasto_obtenido = controlador_finanzas.obtener_gasto_fijo(id_gastosfijos)
+            if gasto_obtenido:
+                return render_template('editar_gasto_fijo.html', gasto=gasto_obtenido)
+            else:
+                return redirect('/gastos_fijos')
+    else:
+        return redirect('/')
+
+@app.route('/eliminar_gasto_fijo/<int:id_gastosfijos>', methods=['GET'])
+def eliminar_gasto_fijo(id_gastosfijos):
+    if controlador_finanzas.eliminar_gasto_fijo(id_gastosfijos):
+        return redirect('/gastos_fijos')
+    else:
+        flash("Error eliminando el gasto fijo", "error")
+        return redirect('/gastos_fijos')
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port = 8000, debug=True)
